cdd45d7480b2a13b4dec7f9351a04f2b085659da
MONA <121730102+MeowDump@users.noreply.github.com>
2026-09-10 16:04:25 +0530

    Add memory actions to CalcAction interface

--- app/src/main/java/com/sosauce/vanilla/data/actions/CalcAction.kt
+++ app/src/main/java/com/sosauce/vanilla/data/actions/CalcAction.kt
@@ -4,6 +4,11 @@ sealed interface CalcAction {
     data object GetResult : CalcAction
     data object ResetField : CalcAction
     data object Backspace : CalcAction
+    data object MemoryClear : CalcAction
+    data object MemoryRecall : CalcAction
+    data object MemoryAdd : CalcAction
+    data object MemorySubtract : CalcAction
+
     data class AddToField(
         val char: Char
     ) : CalcAction
